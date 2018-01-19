a6d1cd499a53cb2012832fc2352642d3b84aedbe
sudhansu <sudhansu@yelp.com>
2018-01-18 17:35:12 -0800

    Initial Commit

---
+++ ImgurRocks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImgurRocks
+//
+//  Created by Sudhansu Singh on 1/18/18.
+//  Copyright © 2018 Sudhansu Singh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
